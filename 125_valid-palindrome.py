a7bc57a22062fa08490172f9bf5d118ec9e90e67
Daiana Tei <daianatei.work@gmail.com>
2025-06-17 11:44:54 +0200

    125. Valid Palindrome. Easy, strings

---
+++ 125_valid-palindrome.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        p1 = 0
+        p2 = len(s) - 1
+
+        # two pointers
+        while p1 < p2:
+            while not s[p1].isalnum():
+                p1 += 1
+                # to handle no alphanum symbol
+                if p1 == len(s) - 1 == p2:
+                    return True
+            while not s[p2].isalnum():
+                p2 -= 1
+
+            # guaranteed alphanums
+            if not (s[p1].lower() == s[p2].lower()):
+                return False
+            p1 += 1
+            p2 -= 1
+
+        return True
