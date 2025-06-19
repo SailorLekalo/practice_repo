f8b868560dde03fe66165a27a223359445971161
maxxbel <71715583+maxxbel@users.noreply.github.com>
2025-06-19 13:33:26 +0300

    Create valid_sudoku.py
    36. Valid Sudoku on leetcode

---
+++ valid_sudoku.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # check rows
+        for i in range(len(board)):
+            dict = {}
+            for j in range(len(board[i])):
+                if board[i][j] == ".":
+                    continue
+                if board[i][j] in dict:
+                    return False
+                dict[board[i][j]] = board[i][j]
+        # check columns
+        for i in range(len(board)):
+            dict = {}
+            for j in range(len(board[j])):
+                if board[j][i] == ".":
+                    continue
+                if board[j][i] in dict:
+                    return False
+                dict[board[j][i]] = board[j][i]
+        # check squares
+        for row in range(3):
+            for col in range(3):
+                dict = {}
+                for i in range(3):
+                    for j in range(3):
+                        if board[row * 3 + i][col * 3 + j] == ".":
+                            continue
+                        if board[row * 3 + i][col * 3 + j] in dict:
+                            return False
+                        dict[board[row * 3 + i][col * 3 + j]] = board[row * 3 + i][col * 3 + j]
+        # if still there the board is valid
+        return True
