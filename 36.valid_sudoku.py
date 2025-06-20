12c2745a45f8f4c002e7076c1099ce592dec0dd1
maxxbel <71715583+maxxbel@users.noreply.github.com>
2025-06-20 11:33:54 +0300

    Update and rename valid_sudoku.py to 36.valid_sudoku.py
    Changes dictionary to be set for more concise syntax

--- 36.valid_sudoku.py
+++ 36.valid_sudoku.py
@@ -2,32 +2,32 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         # check rows
         for i in range(len(board)):
-            dict = {}
+            dict = set()
             for j in range(len(board[i])):
                 if board[i][j] == ".":
                     continue
                 if board[i][j] in dict:
                     return False
-                dict[board[i][j]] = board[i][j]
+                dict.add(board[i][j])
         # check columns
         for i in range(len(board)):
-            dict = {}
+            dict = set()
             for j in range(len(board[j])):
                 if board[j][i] == ".":
                     continue
                 if board[j][i] in dict:
                     return False
-                dict[board[j][i]] = board[j][i]
+                dict.add(board[j][i])
         # check squares
         for row in range(3):
             for col in range(3):
-                dict = {}
+                dict = set()
                 for i in range(3):
                     for j in range(3):
                         if board[row * 3 + i][col * 3 + j] == ".":
                             continue
                         if board[row * 3 + i][col * 3 + j] in dict:
                             return False
-                        dict[board[row * 3 + i][col * 3 + j]] = board[row * 3 + i][col * 3 + j]
+                        dict.add(board[row * 3 + i][col * 3 + j])
         # if still there the board is valid
         return True
