08640e480763b95a25137828be8efd1451ab17ab
Daiana Tei <daianatei.work@gmail.com>
2025-06-24 11:11:27 +0200

    easy. queue

---
+++ 933_NumberOfRecentCalls.py
@@ -0,0 +1,26 @@
+import collections
+
+class RecentCounter(object):
+
+    def __init__(self):
+        self.recent_calls = collections.deque()
+
+
+    def ping(self, t):
+        """
+        :type t: int
+        :rtype: int
+        """
+        self.recent_calls.append(t)
+        while t - self.recent_calls[0] > 3000:
+            self.recent_calls.popleft()
+        return len(self.recent_calls)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
+
+# Runtime: O(1)
+# Memory: O(N)
+
